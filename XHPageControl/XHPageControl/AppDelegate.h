8b5f5dd7d100d8a90408da1821ed8db987618799
周烜合 <zhouxuanhe@runxsports.com>
2019-01-02 16:32:03 +0800

    初始化

---
+++ XHPageControl/XHPageControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XHPageControl
+//
+//  Created by xuanhe on 2019/1/2.
+//  Copyright © 2019 xuanhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XHPageControl/XHPageControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XHPageControl
+//
+//  Created by xuanhe on 2019/1/2.
+//  Copyright © 2019 xuanhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
